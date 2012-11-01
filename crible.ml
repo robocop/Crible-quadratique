f093ae94fe6bd2f757108bc0a39301283ea598a8
Quentin C <quentincormier@gmail.com>
2012-11-01 23:54:08 +0100

    Ajout du pivot

--- crible.ml
+++ crible.ml
@@ -1,5 +1,16 @@
 let l n =  let n = float_of_int n in exp ((log n *. log (log n)) ** (0.5));;
 
+let array_find i0 f arr =
+  let t = Array.length arr in
+  let rec loop i = 
+    if i >= t then raise Not_found
+    else if f (arr.(i)) then i
+    else loop (i+1)
+  in
+  loop i0
+;;
+
+
 let crible n = 
   let tbl = Array.make (n+1) true in
   let i = ref 2 in
@@ -56,11 +67,62 @@ let matrix_id n = let m = Array.create_matrix n n 0 in for i = 0 to n-1 do m.(i)
 let add_ligne m i j = 
   for k = 0 to Array.length m.(0) -1 do
     m.(i).(k) <- (m.(i).(k) + m.(j).(k)) mod 2;
-  done;
-  m
+  done
 ;;
 
-let m = matrix_id 5;;
+let ligne_nulle l = 
+  let t = Array.length l in
+  let rec test i = 
+    if i >= t then true
+    else if l.(i) <> 0 then false
+    else test (i+1)
+  in
+  test 0
+;;
+
+let swap m i j = 
+  let r = m.(i) in
+  m.(i) <- m.(j);
+  m.(j) <- r
+;;
+(* Trouve une combinaison lineaire *)
+let rec pivot m t (i0, j0) = 
+  Printf.printf "i0 = %d, j0 = %d\n" i0 j0;
+  if i0 >= Array.length m  || j0 >= Array.length (m.(0))  then failwith "impossible de trouver une combinaison linéaire"
+  else
+  (* On cherche la première ligne i (i >= i0) de M telle que l.(j0) <> 0 *)
+  try
+    let i = array_find i0 (fun l -> l.(j0) <> 0) m in
+    (* Si elle existe, on échange la ligne i et la ligne i0 *)
+    swap m i i0;
+    Printf.printf "echange de la ligne %d et %d\n" i i0;
+    for i = i0 + 1 to Array.length m -1 do
+      if m.(i).(j0) <> 0 then
+	begin
+	  Printf.printf "L%d <- L%d + L%d\n" i i i0;
+	  add_ligne m i i0;
+	  add_ligne t i i0
+	end
+    done;
+    pivot m t (i0+1, j0+1)
+
+  with _ -> 
+    print_endline "colonne nulle";
+    (* On regarde si une ligne de M n'est pas nulle *)
+    (try (array_find 0 ligne_nulle m, m, t)
+     with Not_found -> pivot m t (i0+1, j0+1) 
+    )
+
+;;
+let m = [|[|0;1;0;1;1|]; 
+	  [|1;1;0;0;0|]; 
+	  [|1;1;1;1;1|]; 
+	  [|0;1;1;0;0|]
+	|]
+;;
+let t = matrix_id 4;;
+
+pivot m t (0,0);;
 add_ligne m 0 0;;
 
 let n = 11351541513;;
